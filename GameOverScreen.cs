37a4c210351f62b5296be454b3465db8a28ac756
Joel Gabriel <joelybahh@gmail.com>
2017-05-10 11:37:17 +1000

    Update GameOverScreen.cs

--- GameOverScreen.cs
+++ GameOverScreen.cs
@@ -1,4 +1,9 @@
-﻿using UnityEngine;
+//Name: GameOverScreen.cs
+//Author: AlySapphire  github.com/AlySapphire
+//                     alyssafackrell.com
+//Purpose Reloads the last played level or quits to main menu
+
+using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
 
